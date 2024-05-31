51ac0970e4ee71244728eac23cebf6e9705cbc0f
gbene <gabri.benedetti@gmail.com>
2024-05-31 11:02:44 +0200

    Better shp2vtk conversion start

--- fracability/utils/general_use.py
+++ fracability/utils/general_use.py
@@ -2,6 +2,8 @@ import scooby
 import pyperclip
 import numpy as np
 import pyvista as pv
+from geopandas import GeoDataFrame
+from vtkmodules.vtkFiltersCore import vtkCleanPolyData
 
 
 def report():
@@ -156,3 +158,77 @@ def setFigLinesBW(fig):
     """
     for ax in fig.get_axes():
         setAxLinesBW(ax)
+
+
+def shp2vtk(df: GeoDataFrame, nodes=False) -> pv.PolyData:
+    """
+    Quickly convert a GeoDataFrame to a PolyData
+    :param df: input GeoDataFrame
+    :param nodes: If the geodataframe are points then set True
+    :return: PolyData.
+
+    Notes
+    ------
+    All the columns of the geodataframe, except for the geomtry column, will be written as cell data
+    """
+
+    get_coord_df = df.geometry.get_coordinates(ignore_index=False, index_parts=True)
+    get_coord_df.reset_index(inplace=True)
+    get_coord_df.columns = ['parts', 'indexes', 'x', 'y']
+    get_coord_df['indexes'] = range(0, len(get_coord_df))
+    get_coord_df['parts'] += 1
+
+    get_coord_df['z'] = np.zeros(len(get_coord_df['x']))
+    get_coord_df['points'] = get_coord_df.loc[:, ['x', 'y', 'z']].values.tolist()
+    get_coord_df['points'] = get_coord_df['points'].map(tuple)
+    duplicate_mask_first = get_coord_df.duplicated(subset=['x', 'y', 'z'], keep='last')
+    first_repeats = get_coord_df[duplicate_mask_first]
+    # other_repeats = get_coord_df[duplicate_mask_other]
+
+    values = first_repeats['indexes'].values
+    keys = first_repeats['points'].values
+
+    unique_values_dict = dict(zip(keys, values))
+
+    # to change the indexes as the ones that are in the dict we get the values from the dict with apply(get) and fill the na with the index column.
+    get_coord_df['indexes'] = get_coord_df['points'].apply(func=lambda x: unique_values_dict.get(x)).fillna(
+        get_coord_df['indexes']).astype(int)
+
+    mem = 0
+    conn = list()
+    for part, index in zip(get_coord_df['parts'], get_coord_df.index):
+        if part != mem:
+            nparts = len(get_coord_df[get_coord_df['parts'] == part])
+            conn.append(nparts)
+        conn.append(get_coord_df.loc[index, 'indexes'])
+        mem = part
+
+    points = np.array(get_coord_df.loc[:, ['x', 'y', 'z']].values.tolist())
+    if nodes:
+        vtk_obj = pv.PolyData(points)
+    else:
+        vtk_obj = pv.PolyData(points, lines=conn)
+
+    arrays = list(df.columns)
+    arrays.remove('geometry')
+    for array in arrays:
+        vtk_obj[array] = df[array].values
+
+
+    # Use CleanPolyData to collapse double points in one
+
+    p = 100000  # Scaling factor needed for the vtk function to work properly
+    vtk_obj.points *= p
+    clean = vtkCleanPolyData()
+    clean.AddInputData(vtk_obj)
+    clean.ToleranceIsAbsoluteOn()
+    clean.ConvertLinesToPointsOff()
+    clean.ConvertPolysToLinesOff()
+    clean.ConvertStripsToPolysOff()
+    clean.Update()
+
+    output_obj = pv.PolyData(clean.GetOutput())
+    output_obj.points /= p  # Rescale back the points
+
+    return output_obj
+
